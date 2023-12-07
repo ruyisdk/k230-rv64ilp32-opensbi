737ffbaded248af0e696dde7b6fec165b1351d42
Yangyu Chen <cyy@cyyself.name>
2023-12-08 03:43:13 +0800

    lib: sbi: detect the number of PMP entries first
    To further probe each PMP entry whether is locked or not, we should
    detect the number of PMP entries first.
    
    Signed-off-by: Yangyu Chen <cyy@cyyself.name>

--- lib/sbi/sbi_hart.c
+++ lib/sbi/sbi_hart.c
@@ -821,6 +821,9 @@ static int hart_detect_features(struct sbi_scratch *scratch)
 	__check_csr_32(__csr + 0, __rdonly, __wrval, __field, __skip)	\
 	__check_csr_32(__csr + 32, __rdonly, __wrval, __field, __skip)
 
+	/* Detect number of PMP regions. At least PMPADDR0 should be implemented*/
+	__check_csr_64(CSR_PMPADDR0, true, 0, pmp_count, __pmp_count_probed);
+__pmp_count_probed:
 	/**
 	 * Detect the allowed address bits & granularity. At least PMPADDR0
 	 * should be implemented.
@@ -829,10 +832,7 @@ static int hart_detect_features(struct sbi_scratch *scratch)
 	if (val) {
 		hfeatures->pmp_log2gran = sbi_ffs(val) + 2;
 		hfeatures->pmp_addr_bits = sbi_fls(val) + 1;
-		/* Detect number of PMP regions. At least PMPADDR0 should be implemented*/
-		__check_csr_64(CSR_PMPADDR0, true, 0, pmp_count, __pmp_skip);
 	}
-__pmp_skip:
 	/* Detect number of MHPM counters */
 	__check_hpm_csr(CSR_MHPMCOUNTER3, mhpm_mask);
 	hfeatures->mhpm_bits = hart_mhpm_get_allowed_bits();
